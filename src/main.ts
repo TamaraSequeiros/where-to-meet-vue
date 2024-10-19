f3bdd6d75fb4502e88c2dad7272877f856425513
Tamara Sequeiros <tamara.sequeiros@gmail.com>
2024-10-19 13:53:19 +0200

    Add favicon

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+// Import the root component App from a single-file component
 import App from './App.vue'
+// UI component library
 import ElementPlus from 'element-plus'
-import VueGoogleMaps from 'vue-google-maps-community-fork'
 import 'element-plus/dist/index.css'
+// Maps
+import VueGoogleMaps from 'vue-google-maps-community-fork'
 
 const app = createApp(App)
 app.use(ElementPlus)
